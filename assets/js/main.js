8475052fb9b0f61f8c73ddd95922f9b98faec273
LMBP <juenpame@gmail.com>
2025-03-02 22:30:13 -0500

    Versión final

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@
 * License: https://bootstrapmade.com/license/
 */
 
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -50,7 +50,7 @@
    * Toggle mobile nav dropdowns
    */
   document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
-    navmenu.addEventListener('click', function(e) {
+    navmenu.addEventListener('click', function (e) {
       e.preventDefault();
       this.parentNode.classList.toggle('active');
       this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
@@ -76,6 +76,7 @@
   function toggleScrollTop() {
     if (scrollTop) {
       window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+      window.scrollY > 7050 ? scrollTop.classList.add('btnFooter') : scrollTop.classList.remove('btnFooter');
     }
   }
   scrollTop.addEventListener('click', (e) => {
@@ -126,7 +127,7 @@
     new Waypoint({
       element: item,
       offset: '80%',
-      handler: function(direction) {
+      handler: function (direction) {
         let progress = item.querySelectorAll('.progress .progress-bar');
         progress.forEach(el => {
           el.style.width = el.getAttribute('aria-valuenow') + '%';
@@ -134,6 +135,36 @@
       }
     });
   });
+
+  /**
+   * Portfolio section display
+   */
+  let serviceLink = document.querySelectorAll('.other-service-item');
+
+  serviceLink.forEach(link => {
+    link.addEventListener("click", function () {
+
+      document.querySelectorAll('.other-service-item').forEach(item => {
+        item.classList.remove("active");
+        item.classList.add("no-active");
+      });
+
+      let listOfClasses = document.getElementById(link.id).classList;
+      listOfClasses.remove("no-active");
+      listOfClasses.add("active");
+
+      document.querySelectorAll('.other-service-item-text').forEach(text => {
+        text.classList.remove("active");
+        text.classList.add("no-active");
+      });
+
+      let targetText = document.getElementById(link.id + '-text');
+      targetText.classList.remove("no-active");
+      targetText.classList.add("active");
+    });
+  });
+
+
 
   /**
    * Initiate Pure Counter
@@ -150,13 +181,13 @@
   /**
    * Init isotope layout and filters
    */
-  document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
+  document.querySelectorAll('.isotope-layout').forEach(function (isotopeItem) {
     let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
     let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
     let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
 
     let initIsotope;
-    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
+    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function () {
       initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
         itemSelector: '.isotope-item',
         layoutMode: layout,
@@ -165,8 +196,8 @@
       });
     });
 
-    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
-      filters.addEventListener('click', function() {
+    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function (filters) {
+      filters.addEventListener('click', function () {
         isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
         this.classList.add('filter-active');
         initIsotope.arrange({
@@ -193,7 +224,7 @@
    * Init swiper sliders
    */
   function initSwiper() {
-    document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+    document.querySelectorAll(".init-swiper").forEach(function (swiperElement) {
       let config = JSON.parse(
         swiperElement.querySelector(".swiper-config").innerHTML.trim()
       );
@@ -211,7 +242,7 @@
   /**
    * Correct scrolling position upon page load for URLs containing hash links.
    */
-  window.addEventListener('load', function(e) {
+  window.addEventListener('load', function (e) {
     if (window.location.hash) {
       if (document.querySelector(window.location.hash)) {
         setTimeout(() => {
